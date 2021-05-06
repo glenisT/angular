da9eb09816059f21a9e233d14fd82ea4e83d3021
User <glentivari@gmail.com>
2021-05-06 13:52:15 +0200

    message

---
+++ angular-tour-of-heroes/src/app/hero.ts
@@ -0,0 +1,8 @@
+export interface Hero{
+    id:number;
+    name:String;
+    attack:number;
+    defense:number;
+    weaknesses:Array<String>;
+    image: String;
+}
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+  {id : 1, name: 'Windstorm',attack: 84,defense : 52,weaknesses: ['Emotional instability, Argonite, Reflective conductors, A big enough electric fan'], image: 'http://1.bp.blogspot.com/_JbjonwYR7Zw/TTDKo5gyS9I/AAAAAAAAAAc/Fq5K_2O95B4/w1200-h630-p-k-nu/hurricanes.jpg'},
+  { id: 2, name: 'Mr. Nice' , attack: 31, defense: 75, weaknesses: ['Bullying'], image: 'https://thumbs.dreamstime.com/b/nerd-businessman-superhero-21190554.jpg'},
+  { id: 3, name: 'Narco', attack: 74, defense: 66, weaknesses: ['The DEA'], image: 'https://iconstyle.al/wp-content/uploads/2017/06/18812359_824111237764397_5338319954343100416_n.jpg'},
+  { id: 4, name: 'Bombasto', attack: 78, defense: 99, weaknesses: ['High cholesterol, Obesity, Donuts, Hamburgers'], image: 'https://s3.scoopwhoop.com/anj/scvsvd/600818401.png'},
+  { id: 5, name: 'Celeritas', attack: 0, defense: 0, weaknesses: ['Salads'], image: 'https://webstockreview.net/images/celery-clipart-animated-1.jpg'},
+  { id: 6, name: 'Magneta', attack: 76, defense: 78, weaknesses: ['Non-metallic materials', 'Same-pole magnets'], image: 'https://tse3.mm.bing.net/th?id=OIP.k1IOmgryftcrNgXQ1qK40wHaD5&pid=Api&P=0&w=314&h=166'},
+  { id: 7, name: 'RubberMan', attack: 68, defense: 55, weaknesses: ['Heat', 'Frost', 'Non-elongated penis size'], image: 'https://forum.y8.com/uploads/default/original/2X/e/e521e1fa389f4dbe8abca56199a140b1eb4110b3.gif'},
+  { id: 8, name: 'Dynama', attack: 80, defense:62, weaknesses: ['Rain, Rust, Blackouts'], image: 'https://tse4.mm.bing.net/th?id=OIP.OOR6p2XsX5nUEwFqYWZB8wHaHa&pid=Api&P=0&w=300&h=300'},
+  { id: 9, name: 'Dr IQ', attack: 62, defense: 80, weaknesses: ['Stupidity, Bullying, His ex wife, Anxiety, Any form of firearm, knife, or heavy blunt object.'], image: 'https://i.kym-cdn.com/entries/icons/facebook/000/030/525/cover5.jpg'},
+  { id: 10, name: 'Magma', attack: 82, defense:65, weaknesses: ['Water'], image: 'https://vignette.wikia.nocookie.net/elminage-gothic/images/8/87/Red_Slime.png/revision/latest?cb=20150418123818'},
+  { id: 11, name: 'Tornado', attack: 75, defense: 64, weaknesses: ['Lack of objects to throw around.'], image: 'http://images3.wikia.nocookie.net/__cb20080619231841/superheroes/images/1/1c/RedTornado-1.jpg'}
+];
